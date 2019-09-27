0288f320a3f3e06d42aeb9d7ff08c58362bb8792
DEBAJYOTI MUKHERJEE <debajmuk@in.ibm.com>
2019-09-27 22:24:01 +0530

    changed

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
 	
 
